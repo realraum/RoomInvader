f09f6304995b0b2d30c1612cde094ef1413bab79
stefan <dev@2904.cc>
2015-05-09 04:26:40 +0200

    added youtube title resolving

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+     render_template, flash, json, jsonify
 
+from urlparse import urlparse, parse_qs
+
+import urllib2
 import subprocess
 
 app = Flask(__name__)
+
+queue = []
+
+def getYouTubeID(videoURL):
+	app.logger.debug('getting videoID: ' + videoURL)
+	return parse_qs(urlparse(videoURL).query)['v'][0]
+
+
+def getYouTubeTitle(videoId):
+	app.logger.debug('getting title for videoId: ' + videoId)
+	response = urllib2.urlopen("http://youtube.com/get_video_info?video_id=" + str(videoId))
+
+	query = urlparse("/?" + response.read()).query
+	 
+	return parse_qs(query)['title'][0]
 
 
 @app.route("/")
@@ -11,15 +29,32 @@ def main():
 	#flash('Hi')
 	return render_template('main.html')
 
+
+@app.route('/queue', methods=['GET'])
+def getQueue():
+	return jsonify(queue=queue)
+
+
 @app.route('/enqueue/youtube', methods=['POST'])
 def enqueue():
+
+	app.logger.debug('/enqueue/youtube ...')
 
 	# this is experimental, replace with mpv.py or ipc something:
 	#os.system("sudo -u realraum mpv --no-video  %s" % request.form['url'])
 	#ret = subprocess.call(["sudo", "-u", "realraum", "mpv", "--no-video", request.form['url']])
-	ret = subprocess.call(["mpv", "--no-video", request.form['url']])
+	
+	# ret = subprocess.call(["mpv", "--no-video", request.form['url']])
 
-	return "OK: %s (%d)" % (request.form['url'], ret)
+	videoID = getYouTubeID(request.form['url'])
+
+	title = getYouTubeTitle(videoID)
+
+	queue.append({    'type': 'YT',
+					 'title': title,
+					'artist': 'YouTube'})
+
+	return "OK: %d" % len(queue)
 
 
 if __name__ == "__main__":
